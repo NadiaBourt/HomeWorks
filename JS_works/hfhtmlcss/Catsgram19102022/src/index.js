63f71adc1a300fd6ee6f8d6863239b4d5873d6fe
golan4ik51 <cheverdin.konst@gmail.com>
2022-10-22 20:20:47 +0300

    HW8

---
+++ JS_works/hfhtmlcss/Catsgram19102022/src/index.js
@@ -0,0 +1,13 @@
+const anchors = document.querySelectorAll(".thumbnails-anchor");
+const detailsImage = document.querySelector(".details-image");
+const detailsTitle = document.querySelector(".details-title");
+function setDetails (anchor) {
+    detailsImage.setAttribute("src", anchor.getAttribute("data-details-image"));
+    detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
+    for (let i = 0; i < anchors.length; i++) {
+        anchors[i].addEventListener("click", function () {
+            setDetails(anchors[i]);
+        })
+    }
+}
+
